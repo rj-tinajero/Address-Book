71e1311357be51bbc7e20a8c8820bb218049f90d
Richard Tinajero <rj.tinajero92@gmail.com>
2019-03-07 09:36:44 -0800

    latest draft

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -29,6 +29,34 @@ module.exports = class ContactController {
                 }
             }
         ];
+        this.searchQuestions = [
+            {
+                type: "input",
+                name: "name",
+                message: "Name of contact to search - ",
+                validate(val) {
+                    return val !== "";
+                }
+            }
+        ];
+        this.showContactQuestions = [
+            {
+                type: "list",
+                name: "selected",
+                message: "Please choose from an option below: ",
+                choices: [
+                    "Delete contact",
+                    "Main menu"
+                ]
+            }
+        ];
+        this.deleteConfirmQuestions = [
+            {
+                type: "confirm",
+                name: "confirmation",
+                message: "are you sure you want to delete this contact?"
+            }
+        ];
     }
 
     addContact(name, phone, email) {
@@ -38,4 +66,44 @@ module.exports = class ContactController {
     getContacts() {
         return Contact.findAll();
     }
+
+    delete(id) {
+        return Contact.destroy({
+            where: {id}
+        })
+    }
+
+    iterativeSearch(contacts, target) {
+        for(let contact of contacts) {
+            if(contact.name.toLowerCase() === target.toLowerCase()) {
+                return contact;
+            }
+        }
+        return null;
+    }
+
+    binarySearch(contacts, target) {
+        let min = 0;
+        let max = contacts.length - 1;
+        let mid;
+        while(min <= max) {
+            mid = Math.floor((min + max) / 2);
+            let currentContact = contacts[mid];
+            if(currentContact.name > target) {
+                max = mid - 1;
+            } else if(currentContact.name < target) {
+                min = mid - 1;
+            } else {
+                return contacts[mid];
+            }
+        }
+        return null;
+    }
+
+    search(name) {
+        return Contact.findOne({
+            where: {name}
+        });
+    }
+
 }
